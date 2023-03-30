b67c6349bf82ddd2d6563cfe4f5b6c8a022ae5ce
Barbara Millon <millon.barbara1@gmail.com>
2023-03-30 14:30:54 +0100

    useEffect added to candidate list

--- the-contractor/src/components/candidateList/CandidateList.js
+++ the-contractor/src/components/candidateList/CandidateList.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { candidates } from "./candidates.js";
+import { useEffect } from "react";
+
 
 export default function CandidateList({ contract }) {
+
+  // based on the contract use useEffect and match candidats to contracts that contain their skills
+  // function takes in contract and returns an array of candidates that match the contract skills
+  // establish an empty array, loop through the candidates array and check if the candidate skills match the contract skills
+  // for each candidate that matches, push the candidate into the empty array
+  // return top 3 candidates 
+
+
+  
+
+
+
+
   return (
     <div>
       <div>
