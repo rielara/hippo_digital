890d252d5949f31297812adc328f5647147834ec
Barbara Millon <millon.barbara1@gmail.com>
2023-03-30 12:24:41 +0100

    contract form populates

--- the-contractor/src/App.js
+++ the-contractor/src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
-import MultiSelectDropdown from "./components/contractForm/ContractForm";
+import ContractForm from "./components/contractForm/ContractForm";
+import { useState } from "react";
+
+
+
 
 function App() {
+
+  const [contract, setContract] = useState([]);
+
+  const updateContract = (newContract) => {
+    setContract([...contract, newContract]);
+    console.log('this is the contract', contract)
+  }
+
+  // this is working but you have to press the submit button twice to get the console log to work
+
+
   return (
     <div className="App">
       <h1>THE CONTRACTORS</h1>
-      <MultiSelectDropdown />
+      <p>Welcome</p>
+      <ContractForm updateContract={updateContract}/>
     </div>
   );
 }
--- the-contractor/src/components/contractForm/ContractForm.js
+++ the-contractor/src/components/contractForm/ContractForm.js
@@ -1,45 +1,68 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const options = [
-  { value: "HTML", label: "HTML" },
-  { value: "CSS", label: "CSS" },
-  { value: "JavaScript", label: "JavaScript" },
-  { value: "Java", label: "Java" },
-  // add more options as needed
-];
+const ContractForm = ({updateContract}) => {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [name, setName] = useState('');
+  const [contract, setContract] = useState('');
+  const [skills, setSkills] = useState([]);
 
-const MultiSelectDropdown = () => {
-  const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const handleChange = (event) => {
-    const { options } = event.target;
-    const selectedValues = [];
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].selected) {
-        selectedValues.push(options[i].value);
-      }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newContract = {
+      startDate: startDate,
+      endDate:endDate,
+      name: name,
+      contract: contract,
+      skills: skills 
     }
-    setSelectedOptions(selectedValues);
+    updateContract(newContract);
+
+    // code to submit contract details
   };
 
+
+  const handleSkillChange = (e) => {
+    const skill = e.target.value;
+    if (e.target.checked) {
+      setSkills([...skills, skill]);
+    } else {
+      setSkills(skills.filter((r) => r !== skill));
+    }
+  };
+
+
+
   return (
-    <div>
-      <label htmlFor="skills">Select your skills:</label>
-      <select
-        id="skills"
-        name="skills"
-        multiple
-        value={selectedOptions}
-        onChange={handleChange}
-      >
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="startDate">Start Date</label>
+      <input type="date" id="startDate" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+
+      <label htmlFor="endDate">End Date</label>
+      <input type="date" id="endDate" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+      
+      <label htmlFor="name">Project Name</label>
+      <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <p>Enter short description of your project.</p>
+      <label htmlFor="contract">Project Description</label>
+      <input type="text" id="contract" value={contract} onChange={(e) => setContract(e.target.value)} />
+      <br/>
+      <label>Select skills you are looking for:</label>
+      <br />
+      <input type="checkbox" value="Java" onChange={handleSkillChange} /> Java
+      <input type="checkbox" value="Javascript" onChange={handleSkillChange} /> Javascript
+      <input type="checkbox" value="Python" onChange={handleSkillChange} /> Python
+      <input type="checkbox" value="AWS" onChange={handleSkillChange} /> AWS
+      <br />
+
+
+      <button type="submit">Find me an ideal candidate</button>
+    </form>
+
+
+
   );
 };
 
-export default MultiSelectDropdown;
+export default ContractForm;
